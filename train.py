b3458338e47e2819e67f54e48c11d78fecbbfb2b
Oliver Tautz <otautz@techfak.uni-bielefeld.de>
2021-05-24 10:28:50 +0200

    something i guess?

--- train.py
+++ train.py
@@ -102,11 +102,11 @@ def train(model, mode, steps, loader, logger):
     simple_logger = SimpleLogger("loss_csv/{}.csv".format(sys.argv[1]),['step','loss','grad_norm','learning_rate'])
 
 
-    no_loops = range(int(steps/ BATCH_SIZE / SEQ_LEN))
-    if sys.argv[2]:
-        no_loops = range(int(sys.argv[2]))
+    #no_loops = range(int(steps/ BATCH_SIZE / SEQ_LEN))
+    #if sys.argv[2]:
+    #    no_loops = range(int(sys.argv[2]))
 
-    for i in tqdm(no_loops):
+    for i in tqdm(range(int(steps/ BATCH_SIZE / SEQ_LEN))):
 
 
         print('batchsize ',BATCH_SIZE)
