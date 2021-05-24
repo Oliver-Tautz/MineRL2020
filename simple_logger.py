1e57c005fba67a9bea4013d76869ec43cbae5ff0
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2021-05-24 12:22:33 +0200

    fix late-logging

--- simple_logger.py
+++ simple_logger.py
@@ -12,17 +12,20 @@ class SimpleLogger():
         self.csv_file= open(filename, 'w', newline='\n')
         self.writer=csv.DictWriter(self.csv_file, colnames, delimiter=';')
         self.writer.writeheader()
-        print("writing header!")
+        self.csv_file.flush()
+
 
 
     def log(self,values):
         assert len(values) == self.no_cols
         tuples = zip(self.colnames,values)
         self.writer.writerow(dict(tuples))
+        self.csv_file.flush()
 
     def log_multiple(self,values_list):
         for values in values_list:
             self.log(values)
+            self.csv_file.flush()
 
     def stop(self):
         self.csv_file.close()
