2366f923efdd337d4a5124e82cf02f28937341fd
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2021-06-21 15:21:20 +0200

    fix mask generation.

--- use_resnet.py
+++ use_resnet.py
@@ -33,9 +33,9 @@ class MaskGeneratorResnet():
         return ((img * IMG_SCALE - IMG_MEAN) / IMG_STD).transpose(0,2).transpose(1,2)
 
     def __prepare_batch(self, batch):
-        IMG_SCALE = torch.tensor(1. / 255)
-        IMG_MEAN = torch.tensor(np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)))
-        IMG_STD = torch.tensor(np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3)))
+        IMG_SCALE = torch.tensor(1. / 255,device=self.device)
+        IMG_MEAN = torch.tensor(np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),device=self.device)
+        IMG_STD = torch.tensor(np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3)),device=self.device)
         return ((batch * IMG_SCALE - IMG_MEAN) / IMG_STD).transpose(1,3).transpose(2,3)
 
     def transform_image(self, img):
